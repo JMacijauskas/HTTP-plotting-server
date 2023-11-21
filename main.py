a3040a32fdbc4f0c20ec65d0e87683b10d2868bb
macijaus <justas.macijauskas.contractor@draeger.com>
2023-11-21 17:33:06 +0200

    Added threading functionality check

--- main.py
+++ main.py
@@ -24,16 +24,25 @@ User POST coordinate -> multi thread server -> Queue -> parse coordinate -> add 
 class Client:
     def __init__(self, target_url: str, plot_type: GraphPlot, data_generator, generator_params: tuple):
         self.generator = data_generator
+        self.generator_initialized = None
         self.url = target_url
         self.single_connection = requests.Session()
         self.generator_params = generator_params
         self.plot_type = plot_type
 
-    def send_points(self, ) -> None:
+    def send_points(self) -> None:
         for coordinate in self.generator(*self.generator_params):
             print(self.single_connection.post(url=self.url, headers={'Graph-Type': self.plot_type.value}, data=coordinate))
 
+    def send_point(self) -> None:
+        if not self.generator_initialized:
+            self.generator_initialized = self.generator(*self.generator_params)
+        print(self.single_connection.post(url=self.url, headers={'Graph-Type': self.plot_type.value}, data=self.generator_initialized.__next__()))
+
 
 if __name__ == '__main__':
-    client = Client('http://localhost:7789', GraphPlot.stairs, function_point_generator, ((-55, 55), lambda x: x + 100000))
-    client.send_points()
+    client1 = Client('http://localhost:7789', GraphPlot.stairs, function_point_generator, ((-50, 50), lambda x: 2*x))
+    client2 = Client('http://localhost:7789', GraphPlot.stairs, function_point_generator, ((-50, 50), lambda x: 2*x + 2))
+    for i in range(50):
+        client1.send_point()
+        client2.send_point()
